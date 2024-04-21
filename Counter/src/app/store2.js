ac7680b8d8c12a1a756cadbe3ee86d81c844a966
wolfiee42 <saifalislam2022@gmail.com>
2024-04-21 08:46:00 +0600

    learned how async function is used in redux. assyncThunk, extraReducer

--- Counter/src/app/store2.js
+++ Counter/src/app/store2.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../../features/counters2/counterSlice2";
+import postsReducer from "../../features/posts/postSlice";
 
 const store2 = configureStore({
   reducer: {
     counters: counterReducer,
+    posts: postsReducer,
   },
 });
 
